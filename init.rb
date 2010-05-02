4f0798db071fc1ab8bcde23da365dfc5155de858
Terence Lee <hone02@gmail.com>
2010-05-02 15:01:34 -0500

    allow specifying of dates

--- init.rb
+++ init.rb
@@ -1,9 +1,18 @@
 class Heroku::Command::Sendgrid < Heroku::Command::Base
   def index
     app  = extract_app
+    date = Hash.new
+
+    args.each do |arg|
+      key, value = arg.split("=")
+      date[key] = value
+    end
+
     vars = heroku.config_vars(app)
     display resource["/monthly_usage"].post(:api_user => vars["SENDGRID_USERNAME"],
-                                            :api_key  => vars["SENDGRID_PASSWORD"])
+                                            :api_key  => vars["SENDGRID_PASSWORD"],
+                                            :year     => date["year"],
+                                            :month    => date["month"])
   end
 
   private
